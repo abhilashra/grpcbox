37054fa1fc9871551dd8f0cadfa5c4de76d8737f
shahid.shaik <shahid.shaik@idt.net>
2026-02-21 10:56:38 +0530

    Removed x-drp-elapsed-ms header to avoid the unknown errors
    Signed-off-by: shahid.shaik <shahid.shaik@idt.net>

--- src/grpcbox_stream.erl
+++ src/grpcbox_stream.erl
@@ -99,8 +99,6 @@ init(Conn, StreamId, [Socket, ServicesTable, AuthFun, UnaryInterceptor,
     {ok, State}.
 
 on_receive_headers(Headers, State=#state{ctx=_Ctx}) ->
-    %% Store request start time for x-drp-elapsed-ms trailer
-    put(grpc_request_start_time, erlang:monotonic_time(microsecond)),
     %% proplists:get_value(<<":method">>, Headers) =:= <<"POST">>,
     Metadata = grpcbox_utils:headers_to_metadata(Headers),
     Ctx = case parse_options(<<"grpc-timeout">>, Headers) of
@@ -344,14 +342,7 @@ end_stream(Status, Message, State=#state{connection=Conn,
                                          stream_id=StreamId,
                                          ctx=Ctx,
                                          resp_trailers=Trailers}) ->
-    %% Calculate elapsed time and add x-drp-elapsed-ms trailer
-    ElapsedMs = case get(grpc_request_start_time) of
-                    undefined -> <<"0">>;
-                    StartTime -> 
-                        integer_to_binary((erlang:monotonic_time(microsecond) - StartTime) div 1000)
-                end,
-    TrailersWithElapsed = [{<<"x-drp-elapsed-ms">>, ElapsedMs} | Trailers],
-    EncodedTrailers = grpcbox_utils:encode_headers(TrailersWithElapsed),
+    EncodedTrailers = grpcbox_utils:encode_headers(Trailers),
     h2_connection:send_trailers(Conn, StreamId, [{<<"grpc-status">>, Status},
                                                     {<<"grpc-message">>, Message} | EncodedTrailers],
                                 [{send_end_stream, true}]),
