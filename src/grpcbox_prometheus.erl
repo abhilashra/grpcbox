b8dc3a911049fecb38fa54d59388c24bf8f4aa20
abhilashra <abhilash.ramadugu@idt.net>
2026-04-07 13:35:57 +0530

    added grpc promethues handler

--- src/grpcbox_prometheus.erl
+++ src/grpcbox_prometheus.erl
@@ -45,8 +45,28 @@ count_rpc_handled(client, Method, Status) ->
 %% Observe RPC latency (in microseconds, like hermes-dyn-router)
 -spec observe_rpc_latency(server | client, binary() | string(), number()) -> ok.
 observe_rpc_latency(server, Method, LatencyMicros) ->
-    catch prometheus_counter:inc(?SERVER_LATENCY_MICS, [Method], round(LatencyMicros)),
+    catch prometheus_histogram:observe(?SERVER_LATENCY_MICS, [Method], LatencyMicros),
+    
+    % Track slow requests (>100ms = 100000 microseconds)
+    case LatencyMicros > 100000 of
+        true ->
+            catch prometheus_gauge:set(grpc_server_last_slow_request_microseconds, 
+                                      [Method], LatencyMicros),
+            catch prometheus_counter:inc(grpc_server_slow_requests_total, [Method]);
+        false ->
+            ok
+    end,
     ok;
 observe_rpc_latency(client, Method, LatencyMicros) ->
-    catch prometheus_counter:inc(?CLIENT_LATENCY_MICS, [Method], round(LatencyMicros)),
+    catch prometheus_histogram:observe(?CLIENT_LATENCY_MICS, [Method], LatencyMicros),
+    
+    % Track slow requests (>100ms = 100000 microseconds)
+    case LatencyMicros > 100000 of
+        true ->
+            catch prometheus_gauge:set(grpc_client_last_slow_request_microseconds, 
+                                      [Method], LatencyMicros),
+            catch prometheus_counter:inc(grpc_client_slow_requests_total, [Method]);
+        false ->
+            ok
+    end,
     ok.
